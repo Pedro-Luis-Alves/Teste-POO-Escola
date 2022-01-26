472582ba1c0c46d44767fb5d0af1d7dd03d9bfd8
Pedro Luis Alves dos Santos <pedrolalves2006@gmail.com>
2022-01-26 14:49:50 -0300

    commit inicial

---
+++ Aluno.java
@@ -0,0 +1,11 @@
+package escola;
+
+public class Aluno {
+
+	String Nome;
+	int idade;
+	int id;
+	int cpf;
+	int serie;
+	String curso;
+}
---
+++ Professor.java
@@ -0,0 +1,8 @@
+package escola;
+
+public class Professor {
+
+	String nome;
+	String materia;
+	int id;
+}
